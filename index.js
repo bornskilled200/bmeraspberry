34eaf17eb37a48495d3e47961ef9d0906706c683
David Park <bornskilled200@gmail.com>
2018-09-10 14:31:09 -0400

    sqlite

--- index.js
+++ index.js
@@ -2,22 +2,28 @@
 
 const { Bme680 } = require('bme680-sensor');
 const bme680 = new Bme680(1, 0x77);
-const { Pool } = require('pg');
+const sqlite = require('sqlite');
 
-const pool = new Pool({
-	host: 'localhost',
-	database: 'bme',
-	user: 'bmewriter',
-	password: 'password',
-});
+const db = await sqlite.open('./bme.db');
+
+await db.run(`CREATE TABLE IF NOT EXISTS conditions (
+  time        INTEGER NOT NULL,
+  uptime      INTEGER NOT NULL,
+  temperature REAL    NOT NULL,
+  humidity    REAL    NOT NULL,
+  air         REAL    NOT NULL,
+  stable      INTEGER NOT NULL
+);`);
+
+const q = await db.prepare('INSERT INTO conditions(time, uptime, temperature, humidity, air, stable) values(?, ?, ?, ?, ?)');
+
 bme680.initialize().then(async () => {
     console.info('Sensor initialized');
     setInterval(async () => {
     	const data = await bme680.getSensorData();
-        console.info(data);
-		const q = 'INSERT INTO conditions(time, location, temperature, humidity, air) values($1, $2, $3, $4, $5)'
-		const values = ['now()', 'room', data.data.temperature, data.data.humidity, data.data.gas_resistance];
-		const { rows } = await pool.query(q, values);
+		const values = ['now()', , data.data.temperature, data.data.humidity, data.data.gas_resistance, data.data.heat_stable ? 1 : 0];
+        console.info(values);
+		q.run(values);
         // INSERT INTO conditions(time, location, temperature, humidity) VALUES (NOW(), 'office', 70.0, 50.0);
     }, 3000);
 });
