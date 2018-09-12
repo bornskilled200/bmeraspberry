193e2b3144c9d14b419e0ed221be929f680ee61a
David Park <bornskilled200@gmail.com>
2018-09-12 17:37:36 -0400

    add cache

--- index.js
+++ index.js
@@ -1,34 +1,23 @@
 'use strict';
 
-const { Bme680 } = require('bme680-sensor');
-const bme680 = new Bme680(1, 0x77);
+// const { Bme680 } = require('bme680-sensor');
+// const bme680 = new Bme680(1, 0x77);
 const sqlite = require('sqlite');
 var express = require('express');
 var path = require('path');
-var compression = require('compression')
+var compression = require('compression');
+var bmeCache = require('./bme-cache.service');
 
-sqlite.open('./bme.db', { cached: true }).then(async db => {
-  await db.run(`CREATE TABLE IF NOT EXISTS conditions (
-    time        INTEGER NOT NULL,
-    uptime      INTEGER NOT NULL,
-    temperature REAL    NOT NULL,
-    humidity    REAL    NOT NULL,
-    air         REAL    NOT NULL,
-    stable      INTEGER NOT NULL
-  );`);
 
-  const q = await db.prepare('INSERT INTO conditions(time, uptime, temperature, humidity, air, stable) values(strftime(\'%s\',\'now\'), ?, ?, ?, ?, ?)');
-    bme680.initialize().then(async () => {
-      console.info('Sensor initialized');
-      setInterval(async () => {
-      const data = await bme680.getSensorData();
-      const values = [process.uptime(), data.data.temperature, data.data.humidity, data.data.gas_resistance, data.data.heat_stable ? 1 : 0];
-      console.info(values);
-      q.run(values);
-      }, 3000);
-  });
-});
-
+// bme680.initialize().then(async () => {
+//   console.info('Sensor initialized');
+//   setInterval(async () => {
+//     const data = await bme680.getSensorData();
+//     const values = [Date.now() / 1000 | 0, process.uptime(), data.data.temperature, data.data.humidity, data.data.gas_resistance, data.data.heat_stable ? 1 : 0];
+//     console.info(values);
+//     bmeCache.write(values);
+//   }, 3000);
+// });
 
 var indexRouter = require('./routes/index');
 var conditionsRouter = require('./routes/conditions');
--- routes/conditions.js
+++ routes/conditions.js
@@ -1,16 +1,14 @@
 var express = require('express');
 var router = express.Router();
 const sqlite = require('sqlite');
+var bmeCache = require('../bme-cache.service');
 
 
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-	sqlite.open('./bme.db', { cached: true, mode: sqlite.OPEN_READONLY }).then(async db => {
-		return db.all('SELECT * FROM conditions ORDER BY time DESC limit 600');
-	}).then(conditions => {
-		res.send((conditions));
-    	res.flush();
+  bmeCache.read().then(array => {
+    res.send(array);
 	});
 });
 
