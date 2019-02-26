15e3a2bc5ad27795e3f886046bcec36a75143272
David Park <bornskilled200@gmail.com>
2019-02-26 19:24:38 +0700

    use pm2.5 sensor

--- index.js
+++ index.js
@@ -2,19 +2,20 @@
 const isPi = process.platform === 'linux'; // VERY NAIVE CHECK
 
 if (isPi) {
-  const { Bme680 } = require('bme680-sensor');
-  const bme680 = new Bme680(1, 0x77);
   const bmeCache = require('./bme-cache.service');
+  const execFile = util.promisify(require('child_process').execFile);
 
-  bme680.initialize().then(async () => {
-    console.info('Sensor initialized');
-    setInterval(async () => {
-      const data = await bme680.getSensorData();
-      const values = [Date.now() / 1000 | 0, process.uptime(), data.data.temperature, data.data.humidity, data.data.gas_resistance, data.data.heat_stable ? 1 : 0];
-      console.info(values);
-      bmeCache.write(values);
-    }, 3000);
-  });
+  async function getAir() {
+    await execFile('/usr/local/lib/airpi/pms5003');
+    const { stdout } = await execFile('/usr/local/lib/airpi/pms5003', ['pm2.5']);
+    return Number(stdout);
+  }
+  console.info('Sensor initialized');
+  setInterval(async () => {
+    const values = [Date.now() / 1000 | 0, process.uptime(), await getAir()];
+    console.info(values);
+    bmeCache.write(values);
+  }, 1000 * 15);
 }
 
 
